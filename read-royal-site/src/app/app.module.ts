5f155bfbb715f292807f304022af5fd284a91c25
roel lucassen <roel.l@live.nl>
2020-09-23 13:17:13 +0200

    Added a main page and a login page without functionality
    Added a basic main page with a navbar + a login form without functionality. Research for security needs to happen

---
+++ read-royal-site/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeScreenComponent,
+    LoginPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ read-royal-site/src/app/home-screen/home-screen.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+declare var jQuery: any;
+
+@Component({
+  selector: 'app-home-screen',
+  templateUrl: './home-screen.component.html',
+  styleUrls: ['./home-screen.component.css']
+})
+export class HomeScreenComponent  {
+
+  constructor() { }
+
+  //ngOnInit(): void {
+   
+  }
+
+
+
+
+
+
+
+
+
